5b1b5a6e8ca62ff7ec38c1ade8ac3bfe6fd5f63b
Ilken Bahcecioglu <ilken.bahcecioglu@playtech.com>
2016-09-14 15:04:38 +0100

    Improved project structure with sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
- 
+var uglify = require('gulp-uglify');
+
 gulp.task('webserver', function() {
   gulp.src('./')
     .pipe(webserver({
@@ -22,16 +23,48 @@ gulp.task('webserver', function() {
     }));
 });
 
+gulp.task('uglifyjs', function() {
+  if(process.env.NODE_ENV === 'development'){
+    gulp.src('src/js/*.js')
+      .pipe(sourcemaps.init())
+      .pipe(uglify({ preserveComments: 'license' }))
+      .pipe(sourcemaps.write('./maps'))
+      .pipe(gulp.dest('dist/js'));
+  }
+  else{
+    gulp.src('src/js/*.js')
+      .pipe(uglify({ preserveComments: 'license' }))
+      .pipe(gulp.dest('dist/js'));
+  }
+});
+
 gulp.task('sass', function () {
-    return gulp.src('./sass/**/*.scss')
+  if(process.env.NODE_ENV === 'development'){
+    gulp.src('./src/sass/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./dist/css'));
+  }
+  else{
+    gulp.src('./src/sass/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'));
+  }
 });
- 
+
 gulp.task('sass:watch', function () {
     gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['webserver', 'sass', 'sass:watch']);
+gulp.task('set-dev-node-env', function() {
+    return process.env.NODE_ENV = 'development';
+});
+
+gulp.task('set-prod-node-env', function() {
+    return process.env.NODE_ENV = 'production';
+});
+
+gulp.task('default', ['set-prod-node-env', 'sass', 'uglifyjs']);
+
+gulp.task('dev', ['set-dev-node-env', 'webserver', 'sass', 'sass:watch', 'uglifyjs']);
